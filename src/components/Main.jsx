bb6f9718d3ea1927c1d027b4fe1b4b207ae8e609
Flash <samimdillinger121@gmail.com>
2022-04-03 13:24:52 +0530

    Modifed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import pizza from '../bg.png'
 function Main() {
   return (
-   <>
-       
-   </>
+    <>
+      <div className='h-screen w-full flex items-center justify-start bg-slate-100 px-10'>
+        <div className='w-1/2 mx-9 h-4/6 flex flex-col items-start justify-center'>
+          <p className='capitalize px-2'>Are you hungry ?</p>
+          <p className='capitalize text-6xl font-bold my-3'>Don't Wait !</p>
+          <button className=" w-24 bg-rose-600 text-white first-letter p-1 px-2 rounded-3xl">Order Now</button>
+        </div>
+        <div className='w-1/2 h-4/6 flex items-center justify-center'>
+          <img src={pizza} alt="" className='w-96 h-96' />
+        </div>
+      </div>
+
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../bg.png';
+import logo from '../logo.png';
 function Navbar() {
   return (
     <div className='h-14 w-full flex items-center justify-between px-7 shadow-lg'>
@@ -7,12 +7,12 @@ function Navbar() {
       <img src={logo} alt=""  className='h-full w-full'/>
       <p>Pizza</p>
     </div>
-    <ul className='flex h-full w-96 items-center justify-between'>
+    <ul className='flex h-full w-80 items-center justify-between'>
       <li>Menu</li>
       <li>Offer</li>
-      <li>MY Orders</li>
+      <li>Orders</li>
       <li>Logout</li>
-      <li>cart</li>
+      <li className='bg-rose-600 p-1 w-12 h-7 flex items-center justify-center rounded-xl text-white'><i className="fa-solid fa-cart-shopping"></i></li>
     </ul>
   </div>
   )
