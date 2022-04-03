664317c09bc80d17da713c698cfa420c9511b82e
Flash <samimdillinger121@gmail.com>
2022-04-03 13:39:05 +0530

    added features

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Main from './components/Main';
 import Navbar from "./components/Navbar"
+import { Router , Routes , Link, Route } from 'react-router-dom';
 function App() {
   return (
     <>
