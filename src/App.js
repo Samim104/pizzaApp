314c2d502b20e97ef0dfedab53f743370aa27d2c
Flash <samimdillinger121@gmail.com>
2022-04-03 18:23:35 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Main from './components/Main';
 import Navbar from "./components/Navbar"
-import { Router , Routes , Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router , Routes , Link, Route } from 'react-router-dom';
 function App() {
   return (
     <>
